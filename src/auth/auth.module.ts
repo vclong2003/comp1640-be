f25f7d6582d7a5412f156ea552d1472fadd43dda
vclong2003 <vclong2003@gmail.com>
2024-03-08 08:58:36 +0700

    local auth login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { PasswordModule } from 'src/shared-modules/password/password.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PasswordModule],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
