b450e9ded599c0eef4c6ad64600bd5b73882dc55
gikhanh <giakhanh1310@gmai.com>
2024-03-11 11:24:23 +0700

    fundametal functions

--- src/event/event.conroller.ts
+++ src/event/event.conroller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { EventService } from './event.service';
+import { CreateEventDTO } from './dtos/create-event.dto'; // Import the missing CreateEventDto
+import { UpdateEventDTO } from './dtos/update-event.dto';
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+  @Post()
+  create(@Body() createEventDto: CreateEventDTO) {
+    return this.eventService.createEvent(createEventDto);
+  }
+  @Get()
+  findAll() {
+    return this.eventService.getAllEvent();
+  }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDTO) {
+    // Fix the parameter name
+    return this.eventService.updateEvent(id, updateEventDto); // Fix the method call
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventService.removeEvent(id);
+  }
+}
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Event } from './schemas/event.schema';
+import { CreateEventDTO } from './dtos/create-event.dto';
+import { UpdateEventDTO } from './dtos/update-event.dto';
+import { FacultyService } from 'src/faculty/faculty.service';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @InjectModel('Event') private eventModel: Model<Event>,
+    private facultyService: FacultyService,
+  ) {}
+
+  async getAllEvent(): Promise<Event[]> {
+    return this.eventModel.find().exec();
+  }
+
+  async createEvent(createEventDto: CreateEventDTO): Promise<Event> {
+    const {
+      name,
+      start_date,
+      first_closure_date,
+      final_closure_date,
+      facultyId,
+    } = createEventDto;
+    // find faculty
+    const faculty = await this.facultyService.findById(facultyId);
+    if (!faculty) {
+      throw new BadRequestException('Faculty not found');
+    }
+
+    // check if event already exists
+    const currentEvent = await this.eventModel
+      .findOne({
+        name: {
+          $regex: new RegExp(`^${name}$`, 'i'),
+        },
+      })
+      .exec();
+    if (currentEvent) {
+      throw new BadRequestException('Event already exists');
+    }
+    // create new event
+    const newEvent = new this.eventModel({
+      name,
+      start_date,
+      first_closure_date,
+      final_closure_date,
+      faculty: {
+        _id: faculty._id,
+        name: faculty.name,
+      },
+    });
+    await newEvent.save();
+    return newEvent;
+  }
+
+  async updateEvent(
+    id: string,
+    updateEventDto: UpdateEventDTO,
+  ): Promise<Event> {
+    const updatedEvent = await this.eventModel
+      .findByIdAndUpdate(id, updateEventDto, { new: true })
+      .exec();
+    if (!updatedEvent) {
+      throw new BadRequestException('Event not found');
+    }
+    return updatedEvent;
+  }
+
+  async removeEvent(id: string): Promise<Event> {
+    const removedEvent = await this.eventModel.findByIdAndDelete(id).exec();
+    if (!removedEvent) {
+      throw new BadRequestException('Event not found');
+    }
+    return removedEvent;
+  }
+}
