9e4cbe5b8b0f23a2d1f39c788421579b038a5afd
vclong2003 <vclong2003@gmail.com>
2024-03-11 11:52:58 +0700

    fixes

--- src/event/dtos/update-event.dto.ts
+++ src/event/dtos/update-event.dto.ts
@@ -1,6 +1,19 @@
+import { IsDate, IsOptional, IsString } from 'class-validator';
+
 export class UpdateEventDTO {
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsDate()
   start_date?: Date;
+
+  @IsOptional()
+  @IsDate()
   first_closure_date?: Date;
+
+  @IsOptional()
+  @IsDate()
   final_closure_date?: Date;
 }
--- src/event/event.conroller.ts
+++ src/event/event.conroller.ts
@@ -13,19 +13,22 @@ import { UpdateEventDTO } from './dtos/update-event.dto';
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
+
   @Post()
   create(@Body() createEventDto: CreateEventDTO) {
     return this.eventService.createEvent(createEventDto);
   }
+
   @Get()
   findAll() {
     return this.eventService.getAllEvent();
   }
+
   @Put(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDTO) {
-    // Fix the parameter name
     return this.eventService.updateEvent(id, updateEventDto); // Fix the method call
   }
+
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.eventService.removeEvent(id);
