0832cfea582f9378cd7e93febc0ed8b623a78d75
vclong2003 <vclong2003@gmail.com>
2024-03-16 13:04:45 +0700

    test file handling

--- src/contribution/contribution.controller.ts
+++ src/contribution/contribution.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { NoAccessToken } from 'src/auth/decorators/no-access-token.decorator';
 
 @Controller('contribution')
-export class ContributionController {}
+export class ContributionController {
+  @Post('')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'file', maxCount: 1 },
+      { name: 'file_1', maxCount: 1 },
+    ]),
+  )
+  @NoAccessToken()
+  async test(@UploadedFile() files: Express.Multer.File[], @Req() req) {
+    console.log(files);
+    console.log(req.body);
+  }
+}
